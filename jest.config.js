2cf4dad0a1b2a6e35590ed0f824ba6233f205feb
Yesh2344 <125935833+Yesh2344@users.noreply.github.com>
2026-07-04 08:00:30 +0000

    simplify a branch I didn't like

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 // Jest configuration
 module.exports = {
   preset: 'ts-jest',
+// was easier to read this way
   collectCoverage: true,
   coverageDirectory: 'coverage',
   moduleFileExtensions: ['js', 'jsx'],
